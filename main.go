ed7aff2bba81ddc1173921010e44f735f9ecadf7
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-30 15:58:11 -0600

    Created a squre root function and added errors and math as imports

--- main.go
+++ main.go
@@ -2,6 +2,8 @@ package main
 
 import ( 
 	"fmt"
+	"errors"
+	"math"
  )
 
 //  func main() {
@@ -85,11 +87,20 @@ import (
 //  }
 
 func main() {
-	result := sum(2, 3)
-	fmt.Println(result)
+	result, err := sqrt(16)
+
+	if err != nil {
+		fmt.Println(err)
+	} else {
+		fmt.Println(result)
+	}
 }
 
 //new function called sum
-func sum(x int, y int) int {
-	return x + y
+func sqrt(x float64) (float64, error) {
+	if x < 0 {
+		return 0, errors.New("Undefined for negative numbers")
+	}
+
+	return math.Sqrt(x), nil
 }
