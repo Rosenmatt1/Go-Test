2a38f33c54fb23022a5d90921590cbb1cef14f0f
Matthew Rosenberg <Rosenmatt1@gmail.com>
2019-05-29 18:01:59 -0600

    Used delete method to remove from a map

--- main.go
+++ main.go
@@ -23,11 +23,25 @@ import (
 // 	}
 //  }
 
-func main() {
-	// var a [5]int
-	// a := [5]int{5,4,3,2,1}
-	a := []int{5,4,3,2,1}
-	a = append(a,13)
-	// a[2] = 7
-	fmt.Println(a)
+// func main() {
+// 	// var a [5]int
+// 	// a := [5]int{5,4,3,2,1}
+// 	a := []int{5,4,3,2,1}
+// 	a = append(a,13)
+// 	// a[2] = 7
+// 	fmt.Println(a)
+//  }
+
+ func main() {
+	vertices := make(map[string]int)
+//Creates key value pairs
+	vertices["triangle"] = 2
+	vertices["square"] = 3
+	vertices["dodecagon"] = 12
+
+	delete(vertices, "square")
+
+	fmt.Println(vertices)   //shows whole array
+	// fmt.Println(vertices["triangle"])  //for a key
+
  }
